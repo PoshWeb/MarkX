9ae786cd55d6e52d2f8501aa556fba23c652c6df
James Brundage <+@users.noreply.github.com>
2026-09-04 12:15:06 -0700

    feat: MarkX.HTML fault tolerance ( Fixes #52 )
    Using #HTML if present

--- Types/MarkX/get_HTML.ps1
+++ Types/MarkX/get_HTML.ps1
@@ -7,8 +7,13 @@
     If the markdown cannot be coerced into XML, this should use ConvertFrom-Markdown.
 #>
 if (-not $this.XML.XHTML) {
-    return $this.Markdown | 
-        ConvertFrom-Markdown | 
-        Select-Object -ExpandProperty Html
+    if ($this.'#HTML') {
+        return "$($this.'#HTML')" + [Environment]::NewLine
+    }
+    else {
+        return $this.Markdown |
+            ConvertFrom-Markdown |
+            Select-Object -ExpandProperty Html
+    }    
 }
 return ("$($this.XML.XHTML.InnerXML)" + [Environment]::NewLine)
