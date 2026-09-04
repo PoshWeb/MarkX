7693dda54fe959c41c565e59276144f33f5e30ac
James Brundage <+@users.noreply.github.com>
2026-09-04 12:00:24 -0700

    feat: MarkX.HTML fault tolerance ( Fixes #52 )

--- Types/MarkX/get_HTML.ps1
+++ Types/MarkX/get_HTML.ps1
@@ -1,2 +1,14 @@
-if (-not $this.XML.XHTML) { return '' }
+<#
+.SYNOPSIS
+    Gets Markdown as HTML
+.DESCRIPTION
+    Gets Markdown as HTML.
+.NOTES
+    If the markdown cannot be coerced into XML, this should use ConvertFrom-Markdown.
+#>
+if (-not $this.XML.XHTML) {
+    return $this.Markdown | 
+        ConvertFrom-Markdown | 
+        Select-Object -ExpandProperty Html
+}
 return ("$($this.XML.XHTML.InnerXML)" + [Environment]::NewLine)
