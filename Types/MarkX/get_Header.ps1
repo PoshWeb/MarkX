445c9c1ec24d28e699ffdaa9c15d7a66635da5f1
James Brundage <+@users.noreply.github.com>
2026-09-08 13:02:10 -0700

    feat: MarkX.Header ( Fixes #91, re #63, re #87, re #69, re #88, re #89 )
    Flexibly getting header

--- Types/MarkX/get_Header.ps1
+++ Types/MarkX/get_Header.ps1
@@ -1,9 +1,36 @@
-if (-not $this.'#YamlHeader') { return }
+<#
+.SYNOPSIS
+    Gets Markdown header
+.DESCRIPTION
+    Gets the Markdown front matter header as an object.
 
-$convertFromYaml = $ExecutionContext.SessionState.InvokeCommand.GetCommand('ConvertFrom-Yaml', 'Alias,Cmdlet,Function')
-if (-not $convertFromYaml) {
-    Write-Warning "Cannot get header without ConvertFrom-Yaml"
-    return
+    If the front matter is JavaScript or no front matter type is detected,
+    will output a `[string]`
+
+    If 
+#>
+if (-not $this.'#FrontMatter') { return }
+
+switch ($this.FrontMatterType) {
+    yaml {
+        $convertFromYaml = $ExecutionContext.SessionState.InvokeCommand.GetCommand('ConvertFrom-Yaml', 'Alias,Cmdlet,Function')
+        if (-not $convertFromYaml) {
+            throw "Cannot get yaml header without ConvertFrom-Yaml"
+        }
+        return ($this.'#FrontMatter' | & $convertFromYaml)
+    }
+    json {
+        return ($this.'#FrontMatter' | ConvertFrom-Json -AsHashtable)
+    }
+    toml {
+        $convertFromToml = $ExecutionContext.SessionState.InvokeCommand.GetCommand('ConvertFrom-Toml', 'Alias,Cmdlet,Function')
+        if (-not $convertFromToml) {
+            throw "Cannot get toml header without ConvertFrom-Toml"            
+        }
+
+        return ($this.'#FrontMatter' | & $convertFromToml)
+    }
+    default {
+        return $this.'#FrontMatter'
+    }
 }
-
-return ($this.'#YamlHeader' | & $convertFromYaml)
