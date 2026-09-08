51e8a1c2f304d8b2b4eaed0da17f5bc6cc187651
James Brundage <+@users.noreply.github.com>
2026-09-07 20:50:18 -0700

    feat: MarkX.Date ( Fixes #58 )
    Auto setting date from path if not already in front matter

--- Types/MarkX/get_Date.ps1
+++ Types/MarkX/get_Date.ps1
@@ -7,10 +7,18 @@
 param()
 
 $header = $this.Header
-if (-not $header.date) { return }
+
 
 if ($header.date -is [DateTime]) {
     return $header.date
 } elseif ($header.date -as [DateTime]) {
     return $header.date -as [DateTime]
+} elseif ($this.Path -match '(?>^|[\\/])\d{4}[-/]\d{2}[-/]\d{2}') {
+    $match = "$($matches.0)"
+    $date = $matches.0 -replace '^[\\/]' -replace '/', '-' -as [DateTime]
+        
+    if ($date) {
+        $this.Date = $date
+        return $date        
+    }    
 }
