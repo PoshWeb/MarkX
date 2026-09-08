c86ba2bb816a072d0d2efdf7875733d405907621
James Brundage <+@users.noreply.github.com>
2026-09-08 14:26:56 -0700

    feat: MarkX.File ( Fixes #86 )
    Filtering out node-modules

--- Types/MarkX/get_File.ps1
+++ Types/MarkX/get_File.ps1
@@ -6,8 +6,12 @@
     
     If the MarkX has a `.Path`, it will get files beneath that path.
     If the MarkX has a `.Site.Root`, it will get files beneath each root.
+.NOTES
+    Will explicitly skip any files in `node-modules`
 #>
 param()
+
+$noise = '[\\/](?>node_modules)'
 
 $enumOptions = [IO.EnumerationOptions]::new()
 $enumOptions.RecurseSubdirectories = $true
@@ -16,14 +20,15 @@ if ($thisPath) {
     if ([IO.File]::Exists($thisPath)) {
         return ($thisPath -as [IO.FileInfo])
     } elseif ([IO.Directory]::Exists($thisPath)) {
-        return ($thisPath -as [IO.DirectoryInfo]).EnumerateFiles('*',$enumOptions)
+        return 
+            @(($thisPath -as [IO.DirectoryInfo]).EnumerateFiles('*',$enumOptions)) -notmatch $noise
     }
 } else {
     if ($this.Site.root) {        
         foreach ($rootDirectory in $this.Site.Root) {
             $rootDirectory = $rootDirectory -as [IO.DirectoryInfo]
             if ($rootDirectory.EnumerateFiles) {
-                $rootDirectory.EnumerateFiles('*',$enumOptions)
+                @($rootDirectory.EnumerateFiles('*',$enumOptions)) -notmatch $noise
             }
         }
     }
