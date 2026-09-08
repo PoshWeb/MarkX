ad9d54502420bfcdc355b00c6d3c563baebf8b11
James Brundage <+@users.noreply.github.com>
2026-09-08 12:35:15 -0700

    feat: MarkX.YamlHeader ( Fixes #69 )
    Almost always getting as yaml, if possible

--- Types/MarkX/get_YamlHeader.ps1
+++ Types/MarkX/get_YamlHeader.ps1
@@ -1,1 +1,48 @@
-return $this.'#YamlHeader'
+<#
+.SYNOPSIS
+    Gets Markdown Yaml Header
+.DESCRIPTION
+    Gets Markdown front matter as a Yaml Header
+
+    If the front matter is already Yaml, will return as-is.
+
+    If the front matter is `[string]`, will return as-is.
+
+    Otherwise, will attempt to `ConvertFrom-Yaml`.
+
+    If `ConvertTo-Yaml` is not installed, will `ConvertTo-Json` instead.
+#>
+switch ($this.FrontMatterType) {
+    yaml {
+        return $this.'#YamlHeader'
+    }
+    default {
+        $header = $this.Header
+        if ($header -is [string]){
+            return $header
+        }
+        $convertToYaml = $ExecutionContext.SessionState.InvokeCommand.GetCommand('ConvertTo-Yaml', 'Alias,Cmdlet,Function')
+        if (-not $convertToYaml) {
+            Write-Warning "ConvertTo-Yaml not found, returning header as json"
+            $header | ConvertTo-Json -Depth 100 
+            return
+        }
+
+        $convertParameters = @{}
+        return try {
+            if ($convertToYaml -and $convertToYaml.Parameters['Depth']) {
+                $convertParameters['Depth'] = $FormatEnumerationLimit
+            }
+        } catch {
+            Write-Verbose "Could not set depth:  Please use YaYaml: $_"    
+        }
+        finally {
+            $header | & $convertToYaml @convertParameters
+            if ($toYaml -is [string]) {
+                $this | Add-Member NoteProperty '#YamlHeader' $toYaml -Force
+                $this | Add-Member NoteProperty '#FrontMatter' $toYaml -Force
+            }
+        }        
+    }    
+}
+return
