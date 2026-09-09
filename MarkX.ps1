8d0495e7e9a6e7aa5fda87ab6ce60f969589908c
James Brundage <+@users.noreply.github.com>
2026-09-09 12:17:19 -0700

    feat: MarkX Shapeshifting and Help ( Fixes #96, Fixes #97, Fixes #98, Fixes #99, Fixes #100, Fixes #101 )

--- MarkX.ps1
+++ MarkX.ps1
@@ -7,7 +7,7 @@
     This allows us to query, extract, and customize markdown.
 .EXAMPLE
     # 'Hello World' In Markdown / MarkX
-    '# Hello World' | MarkX
+    # Hello World' | MarkX
 .EXAMPLE
     # MarkX is aliased to Markdown
     # 'Hello World' as Markdown as XML
@@ -39,8 +39,24 @@
     Get-Help Get-MarkX | 
         MarkX | 
         Select-Object -ExpandProperty Code
+.INPUTS
+    `[Object]`
+
+    This accepts any object from the pipeline and any number of arguments    
+.OUTPUTS
+    Directory inputs will become a site.
+
+    File inputs will become a `MarkX` object.
+.NOTES
+    MarkX can be attached to any type.
+
+    All we need to do is add `MarkX` to the `.pstypenames` of an object.
+
+    When we provide a `[IO.FileInfo]` to MarkX, it is still a `[IO.FileInfo]`.
+    When we provide a `[ScriptBlock]` to MarkX, it is still a `[ScriptBlock]`.
+    When we provide a `[Management.Automation.CommandInfo]` to MarkX, it is still a `[Mangement.Automation.CommandInfo]`.
 #>
-[Alias('Markdown','Get-Markdown')]    
+[Alias('Markdown','Get-Markdown')]
 param()
 
 # Collect all input and arguments.
@@ -54,9 +70,21 @@ $allInput = @($input) + $(if ($args) {
 
 # So we need to make a pass over each input
 $remainingInput = @()
-$inputFiles = @()
+$inputObjects = @()
+
 $markXProtoType = [PSCustomObject]@{PSTypename='MarkX'}
-foreach ($in in $AllInput) {
+
+:nextInput foreach ($in in $AllInput) {
+    if (
+        $in -is [Management.Automation.CommandInfo]
+    ) {
+        $inputObjects += $in
+        continue nextInput
+    }
+
+    if ($in -is [Management.Automation.PathInfo]) {
+        $in = "$in"
+    }
     # If the input starts as slashes
     $inFile = if ($in -match '^\.[\\/]') {
         # get the unresolved path and attempt to cast it to a file
@@ -68,10 +96,19 @@ foreach ($in in $AllInput) {
 
     # If the file was empty (or null)
     if (-not $inFile.Length) {
-        # we will treat it as plain markdown
-        $remainingInput += $in
-        continue # and should continue.
+        # check to see if it is a directory.
+        if ([IO.Directory]::Exists("$inFile")) {
+            # If it was, we will shapeshift the directory into MarkX            
+            $inputObjects += [IO.DirectoryInfo]"$inFile"            
+            continue nextInput  
+        } else {
+
+            # we will treat it as plain markdown
+            $remainingInput += $in
+            continue nextInput # and should continue.
+        }        
     }
+    
     
     # If the file does not exist
     if (-not [IO.File]::Exists($inFile.FullName)) {
@@ -79,15 +116,21 @@ foreach ($in in $AllInput) {
         $remainingInput += $in
         continue # and should continue.
     }
+
+    if ($inFile.Extension -eq '.ps1') {                
+        $in = $ExecutionContext.SessionState.InvokeCommand.GetCommand($inFile.FullName, 'ExternalScript')        
+        $inputObjects += $in        
+        continue nextInput
+    }
     
     # If the file was not markdown or mdx.
     if ($inFile.Extension -notin $markXProtoType.MarkdownExtension) {
         # we will treat the input as markdown
         $remainingInput += $in
         continue # and should continue.
-    }
+    }    
 
-    $inputFiles += $inFile    
+    $inputObjects += $inFile    
 }
 
 # If we have a number of input files,
@@ -96,21 +139,37 @@ $progress = [Ordered]@{id=[random]::new().Next()}
 $progress.Activity = "Converting Markdown"
 
 # Walk over each of our input files
-for ($inputNumber = 0; $inputNumber -lt $inputFiles.Count; $inputNumber++) {
+for ($inputNumber = 0; $inputNumber -lt $inputObjects.Count; $inputNumber++) {
+
+    $inputObject = $inputObjects[$inputNumber]
     # determine percent complete
-    $progress.PercentComplete = $inputNumber * 100 / $inputFiles.Length
-    $progress.Status = $inputFiles[$inputNumber].Name
+    $progress.PercentComplete = $inputNumber * 100 / $inputObjects.Length
+    $progress.Status = $inputObject.Name
     # and write a progress message.
     Write-Progress @progress
 
-    # Then, create a new MarkX object
-    $markx = New-Object PSObject -Property @{
-        PSTypeName = 'MarkX'        
-    }
-    # set its input to the input file
-    $markx.Input = $inputFiles[$inputNumber]
-    # and output the object
-    $markx
+    if (
+        $inputObject -is [IO.DirectoryInfo] -or
+        $inputObject -is [IO.FileInfo] -or 
+        $inputObject -is [Management.Automation.CommandInfo] -or                
+        $inputObject -is [ScriptBlock] 
+    ) {
+        $inputObject.pstypenames.insert(0,'MarkX')
+        $inputObject.Input = $inputObject
+        $inputObject |            
+            Add-Member NoteProperty Invocation $MyInvocation -Force -PassThru
+    }    
+    else {
+        # Then, create a new MarkX object
+        $markx = New-Object PSObject -Property @{
+            PSTypeName = 'MarkX'
+            Invocation = $MyInvocation
+        }
+        # set its input to the input file.
+        $markx.Input = $inputObjects[$inputNumber]
+        # and output the object
+        $markx
+    }    
 }
 
 # If we had any input
@@ -126,6 +185,7 @@ if (-not $remainingInput.Length) { return }
 # Otherwise, create a property bag for the Markdown.
 $markx = New-Object PSObject -Property @{
     PSTypeName = 'MarkX'
+    Invocation = $MyInvocation
 }
 # set its input to all remaining input
 $markx.Input = $remainingInput
